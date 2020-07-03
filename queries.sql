e791295bee64c5fca67479b7f1a4ffabe06442e0
iainxiao <iainxiao@gmail.com>
2020-07-03 16:01:53 -0500

    Task 1 complete

--- queries.sql
+++ queries.sql
@@ -1,4 +1,10 @@
 ## Part 1: Test it with SQL
+COLUMN      DATA TYPE
+id             INT
+employer     VARCHAR
+name         VARCHAR
+skills       VARCHAR
+
 
 ## Part 2: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Field may not be empty")
+    @Size(min = 2, max = 100)
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
