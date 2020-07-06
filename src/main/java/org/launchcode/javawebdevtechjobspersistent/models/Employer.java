22acc5f1c2b61ce62129a5f3d10235532361de60
iainxiao <iainxiao@gmail.com>
2020-07-06 18:10:01 -0500

    Task 2 complete

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,18 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Field may not be empty")
-    @Size(min = 2, max = 100)
+    @Size(min = 1, max = 50, message = "Entry must be between 1-50 characters")
     private String location;
+
+    public Employer() {
+    }
 
     public String getLocation() {
         return location;
